4c26ea95052453b2117e32042a2ec8cc987614d8
niko <nikofigit@gmail.com>
2022-11-29 20:22:59 +0700

    master

---
+++ assets/website.js
@@ -0,0 +1,16 @@
+/* Tanggal Bocoran */
+$(document).ready(function(){
+  var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+  var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
+
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth();
+  var thisDay = date.getDay(),
+      thisDay = myDays[thisDay];
+  var yy = date.getYear();
+  var year = (yy < 1000) ? yy + 1900 : yy;
+
+
+  $('#date-now').html(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
+});
